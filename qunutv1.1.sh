29fde63430dd440a0008d42d4ef28277193a0f18
Luis Adha <40796508+luisadha@users.noreply.github.com>
2022-10-25 12:49:54 +0700

    Create qunutv1.1.sh

---
+++ qunutv1.1.sh
@@ -0,0 +1,39 @@
+#!/system/bin/sh
+
+#Author:  Luis Adha
+# This code is released to the public domain.
+# The author gave permission to use this code snippet in your script. # Script name: qunut.sh # Version: v0.1 # Build date: Thu Dec 16 13:19:21 WIB 2021
+# Info : 39 Lines, 3 Pages.
+more $0
+
+: BEGIN
+# To memorize Surah Qunut /UNTUK MENGHAFAL QUNUT (type: q for Quit/keluar)
+: Days 1st /Hari ke-1
+## Allaahummahdinii fii man hadaiit, wa 'aafinii fii man 'aafaiit,
+
+## Wa tawallanii fii man tawallaiit,
+
+## Wa baarik lii fiimaa a'thaiit.
+
+: Days 2nd /Hari ke-2
+## Wa qinii birahmatika syarra maa qadhaiit.
+
+: Days 3rd /Hari ke-3
+## Fa innaka taqdhii wa laa yuqdhaa 'alaiik.
+
+: Days 4th /Hari ke-4
+## Wa innahuu laa yadzillu man waalaiit.
+
+: Days 5th /Hari ke-5
+## Wa laa ya'izzu man 'aadaiit.
+
+: Days 6th /Hari ke-6
+## Tabaarakta rabbannaa wa ta'aalaiit.
+
+: Days 7th /Hari ke-7
+## Fa lakal-hamdu 'alaa maa qadhaiit astaghfiruka wa atuubu ilaiik.
+
+: Days 8th /Hari ke-8
+## Wa shallallaahu 'alaa sayyidinaa Muhammadin-nabiyyil-ummiyyi wa 'alaa aalihii wa shahbihii wa sallam.
+
+: EOF 
