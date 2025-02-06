eb30178bb239d35e186318a2e8f1d954e831878a
Luis Adha <40796508+luisadha@users.noreply.github.com>
2025-02-07 02:45:54 +0700

    Update qunut.sh

--- qunut.sh
+++ qunut.sh
@@ -1,7 +1,8 @@
-#!/usr/bin/bash
-
-#/system/bin/sh
-
+#<html><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>alrc.luisadha.my.id</title><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.2.3/gh-fork-ribbon.min.css" /><link rel="stylesheet" href="https://unpkg.com/@highlightjs/cdn-assets@11.5.0/styles/default.min.css"><script src="https://unpkg.com/@highlightjs/cdn-assets@11.5.0/highlight.min.js"></script><script> hljs.initHighlightingOnLoad();</script><style>body { font-family: monospace; background: rgba(245, 245, 245, 1); } footer { font-size: 0.65rem; } a { color: inherit; text-decoration: none; } pre, code { border-radius: 4px; font-size: 1.1rem; color: #F0F0F0 !important; background: rgb(11 12 14) !important; } .github-fork-ribbon:before { background-color: rgb(11 12 14); }</style></head><body>: weapon-url-opener(1) installer<a class="github-fork-ribbon" href="https://github.com/luisadha/alrc-termux" data-ribbon="Contribute on GitHub" title="Contribute on GitHub">Contribute on GitHub</a> <h2>
+#!/usr/bin/env bash
+#</h2><span>luisadha.github.io/weapon-url-opener Use this self-hosted script to install your weapon-url-opener.</span><h3>
+### <a name="usage" href="#usage">INSTALL</a></h2><pre><code code="shell">gh repo clone luisadha/weapon-url-opener</code></pre><h3>
+### <a name="code" href="#code">CODE</a></h2><pre><code code="bash">
 #Author:  Luis Adha
 # This code is released to the public domain.
 # The author gave permission to use this code snippet in your script. # Script name: qunut.sh # Version: v0.1 # Build date: Thu Dec 16 13:19:21 WIB 2021
@@ -39,3 +40,4 @@ more $0
 ## Wa shallallaahu 'alaa sayyidinaa Muhammadin-nabiyyil-ummiyyi wa 'alaa aalihii wa shahbihii wa sallam.
 
 : EOF 
+#</code></pre><footer>#### Copyright (c) 2025 <a href="https://luisadha.my.id">luisadha.my.id</a> (<a href="https://github.com/luisadha">github.com/luisadha</a>) ####</footer></body></html>
