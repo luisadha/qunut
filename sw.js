28e20b685b4594a897dfba45df8a5533ccb78744
TENET <40796508+luisadha@users.noreply.github.com>
2026-04-23 21:18:04 +0800

    Update hostname whitelist in service worker

--- sw.js
+++ sw.js
@@ -23,8 +23,7 @@ const PRECACHE_LIST = [
 ]
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
-  "huangxuan.me",
-  "yanshuo.io",
+  "luisadha.github.io",
   "cdnjs.cloudflare.com"
 ]
 const DEPRECATED_CACHES = ['precache-v1', 'runtime', 'main-precache-v1', 'main-runtime']
